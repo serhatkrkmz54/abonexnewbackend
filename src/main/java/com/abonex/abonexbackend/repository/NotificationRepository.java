048db375a8e4165f761a95c4952aa89895d372e5
serhatkrkmz54 <korkmaz_serhat@hotmail.com>
2025-09-25 15:38:10 +0300

    25.09.2025

--- src/main/java/com/abonex/abonexbackend/repository/NotificationRepository.java
+++ src/main/java/com/abonex/abonexbackend/repository/NotificationRepository.java
@@ -5,13 +5,17 @@ import com.abonex.abonexbackend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
     List<Notification> findByUserOrderByCreatedAtDesc(User user);
     long countByUserAndIsReadFalse(User user);
-    @Modifying
+
+    @Transactional
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
     @Query("UPDATE Notification n SET n.isRead = true WHERE n.user = :user AND n.isRead = false")
-    void markAllAsReadForUser(User user);
+    void markAllAsReadForUser(@Param("user") User user);
 }
